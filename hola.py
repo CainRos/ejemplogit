429416dc13c0c6778ac869499fab4afb358a5d74
CainRos <za20011804@zapopan.tecmm.edu.mx>
2020-09-08 17:24:03 -0600

    Initial commit

---
+++ hola.py
@@ -0,0 +1,5 @@
+p1=input("¿Cual es tu nombre?\n")
+p2=input("¿Cuales son tus apellidos?\n")
+p3=input("¿Cual es tu numero de control?\n")
+
+print("hola, ",p1 +" "+p2,"¿así que tu número de control es: "+p3+"?")
